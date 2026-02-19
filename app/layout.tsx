d6a8dd1cd7b4b6e0b9c501fb65fb36eebd7065c8
SergioVera <sergio.vera@icloud.com>
2026-02-19 00:48:50 -0400

    add layout

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: "EF Demo",
+  description: "Emporium Factory Demo",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
